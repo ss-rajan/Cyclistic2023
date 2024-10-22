ef3eb6535b2eee25d6f2fb4e5e30697961131d7e
ss-rajan <siddharthrajan164@gmail.com>
2024-10-22 19:14:44 +0300

    Data Cleaning query.

--- Cleaning_data.sql
+++ Cleaning_data.sql
@@ -1,3 +1,20 @@
+-- Removing Null values from cyclistic2023
+
+DELETE FROM cyclistic2023 c 
+WHERE  ride_id = ''
+OR rideable_type = ''
+OR started_at IS NULL 
+OR ended_at IS NULL 
+OR start_station_name = ''
+OR start_station_id = ''
+OR end_station_name = ''
+OR end_station_id = ''
+OR start_lat IS NULL 
+OR start_lng IS NULL 
+OR end_lat IS NULL 
+OR end_lng IS NULL
+OR member_casual = ''
+
 -- Now that there are  no more null values or empty string to find out duplicates or inconsistencies
 
 SELECT COUNT(ride_id) - count(DISTINCT ride_id) AS dup_ride_id
@@ -44,7 +61,6 @@ end_lat IS NULL OR
 end_lng IS NULL
 
 -- No null values for all the given columns when data was explored to find null values / discrepancies
-
 
 
 DELETE FROM cyclistic2023 c
